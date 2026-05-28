7a885ffdbde066bb14473913882255914fe719c9
AkashDas2005 <akashdass1968@gmail.com>
2026-05-28 12:11:43 +0530

    New files for CSS added

---
+++ JS/Introduction/script.js
@@ -0,0 +1,73 @@
+//Introduction
+// console.log("Hello World")
+// console.warn("Warning")
+// console.error("Error")
+
+// alert('This is a alert message')
+
+// var a = confirm('Are you sure about that?') 
+// console.log(a)
+
+// var user = prompt('Enter your name')
+// console.log(user)
+
+//If-else
+// var a = prompt("Enter the number")
+
+// if(a%2!=0){
+//     console.log(`$a is odd number`)
+// }
+
+// else{
+//     console.log(`$a is a even number`)
+// }
+
+//Switch-case
+//  var b = Number(prompt("Enter the number for a weekday"))
+
+// switch(b){
+//     case 1:
+//         console.log("It's Monday");
+//         break;
+//     case 2:
+//         console.log("It's Tuesday");
+//         break;
+//     case 3:
+//         console.log("It's Wednesday");
+//         break;
+        
+//     case 4:
+//         console.log("It's Thursday");
+//         break;
+//     case 5:
+//         console.log("It's Friday");
+//         break;
+//     case 6:
+//         console.log("It's Saturday");
+//         break; 
+//     case 7:
+//         console.log("It's Sunday");
+//         break;    
+//     default:
+//         console.log("It's not a relevant number")           
+// }
+
+//Loop
+
+//while loop
+// var n = Number(prompt("Enter the number: "));
+// var i=1;
+// var a=1;
+// while(i>=1 && i<=10){
+//     a = n*i;
+//     console.log(`${n} x ${i} = ${a}`);
+//     i+=1;
+// }
+
+//Do-While loop
+// password = "123"
+// do{
+//     var n = prompt("Enter the password: ");
+// }while(n!="123");
+
+// console.log("Welcome ")
